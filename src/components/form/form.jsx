beaf81fb05973bca3be97f04a1f21b2ec64bd36f
evgen <evgen_abramovich@mail.ru>
2023-02-11 00:27:22 +0800

    добавил страницу инпутов

---
+++ src/components/form/form.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import '../form/index.css'
+
+export const Form = ()=>{
+
+    const [contactInfo, setContactInfo] = useState({
+        name: '',
+        lastName: '',
+        phoneNamber: ''
+    })
+
+    const handleChange = (e)=>{
+        setContactInfo({...contactInfo, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        console.log(e)
+        e.preventDefault()
+        console.log(contactInfo)
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className='form__container'>
+            <h3>Введите данные</h3>
+            <input className='form' type='text' name='name' placeholder='Имя' onInput={handleChange} />
+            <input className='form' type='text' name='lastName' placeholder='Фамилия' onInput={handleChange} />
+            <input className='form' type='number' name='phoneNamber' placeholder='Телефон' onInput={handleChange} />
+            <button className='button'>Отправить</button>
+        </form>
+    )
+}
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,6 +4,7 @@ import { Favorite } from "../pages/favorites/favoretes"
 import { NoMatchFound } from "../pages/NoMatchFound/NoMatchFound"
 import { CatalogPage } from "../pages/product/catalog/catalog"
 import { ProductPage } from "../pages/product/product"
+import { Form } from '../components/form/form'
 
 export const Router = ({ handleProductLike })=> {
    
@@ -17,6 +18,7 @@ export const Router = ({ handleProductLike })=> {
           <Route path='/product/:productId' element={<ProductPage />}></Route>
           <Route path='/favorite' element={<Favorite />} />
           <Route path="/faq" element={<FaqPage />}></Route>
+          <Route path="/form" element={<Form />}></Route>
           <Route path='*' element={<NoMatchFound />}></Route>
           
         </Routes>
